daf8214ca6311e23b221da6464051a44c0a5fcf8
Subhadeep Pramanik <51330833+adrianalvird@users.noreply.github.com>
2024-08-11 08:37:56 +0530

    Create http_client.py

---
+++ utils/http_client.py
@@ -0,0 +1,8 @@
+# utils/http_client.py
+
+import requests
+
+def send_request(url, params=None, headers=None):
+    """Sends an HTTP GET request and returns the response."""
+    response = requests.get(url, params=params, headers=headers)
+    return response
