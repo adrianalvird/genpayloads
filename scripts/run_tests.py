1e7d5d42d26e0d5f202e27ef411ba80dc3114c19
Subhadeep Pramanik <51330833+adrianalvird@users.noreply.github.com>
2024-08-11 08:40:42 +0530

    Create run_tests.py

---
+++ scripts/run_tests.py
@@ -0,0 +1,9 @@
+# scripts/run_tests.py
+
+import unittest
+from tests.test_payloads import TestPayloads
+from tests.test_mutation import TestMutation
+from tests.test_integration import TestIntegration
+
+if __name__ == "__main__":
+    unittest.main()
